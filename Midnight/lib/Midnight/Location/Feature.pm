7328f087d2d8cc86aacd9f0387b98bb1d728755d
Robert Norris <rob@cataclysm.cx>
2006-08-05 14:04:49 +0000

    now starring!
    git-svn-id: svn+ssh://cataclysm.cx/goodnight@9 d6273d0a-c518-0410-a558-a98191425f41

---
+++ Midnight/lib/Midnight/Location/Feature.pm
@@ -0,0 +1,21 @@
+package Midnight::Location::Feature;
+
+use Class::Constant
+    MOUNTAIN     => "mountains",
+    CITADEL      => "citadel",
+    FOREST       => "forest",
+    HENGE        => "henge",
+    TOWER        => "tower",
+    VILLAGE      => "village",
+    DOWNS        => "downs",
+    KEEP         => "keep",
+    SNOWHALL     => "snowhall",
+    LAKE         => "lake",
+    FROZEN_WASTE => "frozen wastes",
+    RUIN         => "ruin",
+    LITH         => "lith",
+    CAVERN       => "cavern",
+    ARMY         => "plains",
+    PLAINS       => "plains";
+		
+1;
