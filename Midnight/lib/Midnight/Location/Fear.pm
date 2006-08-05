df0e22d4a835df4a9403b3a52dbd1d0ad686ea38
Robert Norris <rob@cataclysm.cx>
2006-08-05 14:01:07 +0000

    be afraid
    git-svn-id: svn+ssh://cataclysm.cx/goodnight@8 d6273d0a-c518-0410-a558-a98191425f41

---
+++ Midnight/lib/Midnight/Location/Fear.pm
@@ -0,0 +1,13 @@
+package Midnight::Location::Fear;
+
+use Class::Constant
+    UTTERLY_COLD  => "utterly cold",
+    VERY_COLD     => "very cold",
+    COLD          => "cold",
+    QUITE_COLD    => "quite cold",
+    SLIGHTLY_COLD => "slightly cold",
+    MILD          => "mild",
+    VERY_MILD     => "very mild",
+    UTTERLY_MILD  => "utterly mild";
+
+1;
