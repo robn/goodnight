961dd22ae721fc3a50698052dac9a8a836a73794
Robert Norris <rob@cataclysm.cx>
2006-08-07 03:50:31 +0000

    what number?
    git-svn-id: svn+ssh://cataclysm.cx/goodnight@13 d6273d0a-c518-0410-a558-a98191425f41

---
+++ Midnight/lib/Midnight/Time.pm
@@ -0,0 +1,80 @@
+package Midnight::Time;
+
+use warnings;
+use strict;
+
+use Class::Std;
+
+use constant DAWN  => 16;
+use constant NIGHT => 0;
+
+my %time    : ATTR( :get<time> );
+
+sub START {
+    my ($self) = @_;
+
+    $self->dawn;
+}
+
+sub dawn {
+    my ($self) = @_;
+
+    $time{ident $self} = DAWN;
+}
+
+sub night {
+    my ($self) = @_;
+
+    $time{ident $self} = NIGHT;
+}
+
+sub increase {
+    my ($self, $increment) = @_;
+
+    $time{ident $self} += $increment;
+    $time{ident $self} = DAWN if $time{ident $self} > DAWN;
+}
+
+sub decrease {
+    my ($self, $decrement) = @_;
+
+    $time{ident $self} -= $decrement;
+    $time{ident $self} = NIGHT if $time{ident $self} < NIGHT;
+}
+
+sub is_dawn {
+    my ($self) = @_;
+
+    return $time{ident $self} == DAWN;
+}
+
+sub is_night {
+    my ($self) = @_;
+
+    return $time{ident $self} == NIGHT;
+}
+
+use overload
+    q{""} => sub {
+        my ($self) = @_;
+
+        if ($time{ident $self} == DAWN) {
+            return "It is dawn.";
+        }
+        if ($time{ident $self} == NIGHT) {
+            return "It is night.";
+        }
+
+        my $lt = "";
+        if ($time{ident $self} % 2 == 1) {
+            $lt = "Less than ";
+        }
+
+        return
+            $lt .
+            ($time{ident $self} / 2 + 1) .
+            " hour" . (($time{ident $self} < 3) ? "" : "s") .
+            "of the day remain";
+    };
+
+1;
