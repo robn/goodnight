84c22f15405d001606267800fecef2fce72fbe79
Robert Norris <rob@cataclysm.cx>
2006-08-07 00:01:28 +0000

    I object!
    git-svn-id: svn+ssh://cataclysm.cx/goodnight@10 d6273d0a-c518-0410-a558-a98191425f41

---
+++ Midnight/lib/Midnight/Location/Object.pm
@@ -0,0 +1,25 @@
+package Midnight::Location::Object;
+
+use Class::Constant
+    NOTHING          => "nothing",
+    WOLVES           => "wolves",
+    DRAGONS          => "dragons",
+    ICE_TROLLS       => "ice trolls",
+    SKULKRIN         => "Skulkrin",
+    WILD_HORSES      => "wild horses",
+    SHELTER          => "shelter and is refreshed",
+    GUIDANCE         => "guidance. A voice calls, ",
+    SHADOWS_OF_DEATH => "the Shadows of Death which drain him of vigour",
+    WATERS_OF_LIFE   => "the Waters of Life which fill him with vigour",
+    HAND_OF_DARK     => "the Hand of Dark which brings death to the day",
+    CUP_OF_DREAMS    => "the Cup of Dreams which brings new welcome",
+    WOLFSLAYER       => "the sword Wolfslayer",
+    DRAGONSLAYER     => "the sword Dragonslayer",
+    ICE_CROWN        => "the Ice Crown",
+    MOON_RING        => "the Moon Ring",
+    FAWKRIN          => "Fawkrin the Skulkrin",
+    FARFLAME         => "Farflame the Dragonlord",
+    LAKE_MIRROW      => "Lake Mirrow",
+    LORGRIM          => "Lorgrim the Wise";
+
+1;
