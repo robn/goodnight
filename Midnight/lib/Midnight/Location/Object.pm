21ed47fd2de21576eec735a62289166fe55ad4ac
Robert Norris <rob@cataclysm.cx>
2006-08-10 10:45:16 +0000

    I have sinned.
    git-svn-id: svn+ssh://cataclysm.cx/goodnight@34 d6273d0a-c518-0410-a558-a98191425f41

--- Midnight/lib/Midnight/Location/Object.pm
+++ Midnight/lib/Midnight/Location/Object.pm
@@ -11,7 +11,7 @@ use Class::Constant
     SKULKRIN         => "Skulkrin",
     WILD_HORSES      => "wild horses",
     SHELTER          => "shelter and is refreshed",
-    GUIDANCE         => "guidance. A voice calls, ",
+    GUIDANCE         => "guidance",
     SHADOWS_OF_DEATH => "the Shadows of Death which drain him of vigour",
     WATERS_OF_LIFE   => "the Waters of Life which fill him with vigour",
     HAND_OF_DARK     => "the Hand of Dark which brings death to the day",
@@ -24,5 +24,38 @@ use Class::Constant
     FARFLAME         => "Farflame the Dragonlord",
     LAKE_MIRROW      => "Lake Mirrow",
     LORGRIM          => "Lorgrim the Wise";
+
+sub is_beast {
+    my ($object) = @_;
+
+    return $object == WOLVES or
+           $object == DRAGONS or
+           $object == ICE_TROLLS or
+           $object == SKULKRIN;
+}
+
+# !!! this is badness of astronomical proportions. its bad enough that these
+# enum classes have additional methods in them. its inexcusable to build game
+# mechanics into them, and its just as bad to abuse a stringification method
+# this way.
+
+sub as_string {
+    my ($object, $location) = @_;
+
+    return $object->SUPER::as_string if $object != GUIDANCE or @_ == 1 or not $location;
+
+    my $msg = "guidance. A voice says: '";
+    my $rnd = $location->get_game->random(32);
+    if ($rnd > 4) {
+        my $character = $location->get_game->get_characters->[$rnd];
+
+        $msg .= "Looking for $character you must seek ".$character->get_location."'";
+    }
+    else {
+        $msg .= $object->by_ordinal($rnd+16) . " can destroy the Ice Crown'";
+    }
+
+    return $msg;
+}
 
 1;
