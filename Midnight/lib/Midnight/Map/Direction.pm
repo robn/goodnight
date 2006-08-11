bbea36d68ca8fbf32aa3a8380959330a6d8d6ea4
Robert Norris <rob@cataclysm.cx>
2006-08-11 22:03:02 +0000

    this reads better
    git-svn-id: svn+ssh://cataclysm.cx/goodnight@63 d6273d0a-c518-0410-a558-a98191425f41

--- Midnight/lib/Midnight/Map/Direction.pm
+++ Midnight/lib/Midnight/Map/Direction.pm
@@ -4,14 +4,14 @@ use warnings;
 use strict;
 
 use Class::Constant
-    NORTH     => "North",     { x =>  0, y => -1 },
-    NORTHEAST => "Northeast", { x =>  1, y => -1 },
-    EAST      => "East",      { x =>  1, y =>  0 },
-    SOUTHEAST => "Southeast", { x =>  1, y =>  1 },
-    SOUTH     => "South",     { x =>  0, y =>  1 },
-    SOUTHWEST => "Southwest", { x => -1, y =>  1 },
-    WEST      => "West",      { x => -1, y =>  0 },
-    NORTHWEST => "Northwest", { x => -1, y => -1 };
+    NORTH     => "north",     { x =>  0, y => -1 },
+    NORTHEAST => "northeast", { x =>  1, y => -1 },
+    EAST      => "east",      { x =>  1, y =>  0 },
+    SOUTHEAST => "southeast", { x =>  1, y =>  1 },
+    SOUTH     => "south",     { x =>  0, y =>  1 },
+    SOUTHWEST => "southwest", { x => -1, y =>  1 },
+    WEST      => "west",      { x => -1, y =>  0 },
+    NORTHWEST => "northwest", { x => -1, y => -1 };
 
 sub turn_right {
     my ($direction) = @_;
