192bbfd8ccba41a55cbce2e15b13fa4dc86fd934
Robert Norris <rob@cataclysm.cx>
2006-10-11 09:12:31 +0000

    can't remember, but I know its fun
    git-svn-id: svn+ssh://cataclysm.cx/goodnight@145 d6273d0a-c518-0410-a558-a98191425f41

--- trunk/web/tiletest/tiletest.js
+++ trunk/web/tiletest/tiletest.js
@@ -4,33 +4,49 @@ var height = 7;
 var tile_width = 100;
 var tile_height = 46;
 
-var edge_gradient = 0.46;
-
 var origin_x = (height-1) * (tile_width/2);
 var origin_y = 0;
 
 var half_width = tile_width/2;
 var half_height = tile_height/2;
 
-function debug (text) {
+function debug (thing) {
+    if (typeof thing == "object" && thing.constructor != Array) {
+        var l = new Array();
+        var i;
+        for (i in thing)
+            l.push(i + ": " + thing[i])
+        debug(l);
+        return;
+    }
+
     var p = document.getElementById("debug");
 
     while (p.hasChildNodes()) {
         p.removeChild(p.lastChild);
     }
 
-    if (typeof text == "string") {
-        p.appendChild(document.createTextNode(text));
+    if (typeof thing == "string") {
+        p.appendChild(document.createTextNode(thing));
     }
 
-    else if (typeof text == "object" && text.constructor == Array) {
-        for (i in text) {
-            p.appendChild(document.createTextNode(text[i]));
+    else if (typeof thing == "object" && thing.constructor == Array) {
+        var i;
+        for (i in thing) {
+            p.appendChild(document.createTextNode(thing[i]));
             p.appendChild(document.createElement("br"));
         }
     }
 }
 
+function tile_to_map (tile_x, tile_y) {
+    /* magic the current tile into an x,y coordinate for the map array */
+    var map_y = Math.floor((tile_y * half_width - tile_x * half_height) / (2 * half_width * half_height));
+    var map_x = Math.floor((tile_x + half_width * map_y) / half_width);
+
+    return [ map_x, map_y ];
+}
+    
 var select;
 
 function page_to_map (page_x, page_y) {
@@ -72,9 +88,6 @@ function page_to_map (page_x, page_y) {
     var tile_x = in_tile1 ? tile1_x : tile2_x;
     var tile_y = in_tile1 ? tile1_y : tile2_y;
 
-    var map_y = Math.floor((tile_y * half_width - page_x * half_height) / (2 * half_width * half_height));
-    var map_x = Math.floor((tile_x + half_width * map_y) / half_width);
-
     if(!select) {
         select = document.createElement("img");
         select.setAttribute("class", "tile");
@@ -85,21 +98,33 @@ function page_to_map (page_x, page_y) {
     select.style.top = tile_y;
     select.style.left = tile_x;
 
-    debug([
-        "[px,py]   = [" + page_x + "," + page_y + "]",
-        "[t1x,t1y] = [" + tile1_x + "," + tile1_y + "]",
-        "[t2x,t2y] = [" + tile2_x + "," + tile2_y + "]",
-        "in t1     = " + in_tile1,
-        "[mx,my]   = [" + map_x + "," + map_y + "]"
-    ]);
+    return tile_to_map(tile_x, tile_y);
+}
 
-    return [ map_x, map_y ];
+var tiles = [ "red.png", "green.png", "yellow.png", "blue.png" ];
+
+var map = new Array(50);
+var p;
+for (p = 0; p < 50; p++)
+    map[p] = new Array(50);
+var t = 0, e = 0;
+while (e < 25) {
+    for (p = e; p < 50 - e; p++) {
+        map[p]  [e] = map[p]  [49-e] = t;
+        map[p-e][e] = map[p-e][49-e] = t;
+        map[e]  [p] = map[49-e][p]   = t;
+        map[e][p-e] = map[49-e][p-e] = t;
+    }
+    t = (t+1) % 4;
+    e++;
 }
 
 function add_tile (map_x, map_y, page_x, page_y) {
     var img = document.createElement("img");
     img.setAttribute("class", "tile");
-    img.setAttribute("src", "blank.png");
+
+    var map_xy = tile_to_map(page_x, page_y);
+    img.setAttribute("src", tiles[map[map_xy[0]][map_xy[1]+10]]);
 
     img.style.left = page_x;
     img.style.top = page_y;
@@ -108,18 +133,18 @@ function add_tile (map_x, map_y, page_x, page_y) {
 }
 
 function setup_tiles () {
-    var x, y;
-    
-    for (x = 0; x < width; x++) {
-        for (y = 0; y < height; y++) {
-            var page_x = origin_x + ((x-y) * half_width);
-            var page_y = origin_y + ((x+y) * half_height);
+    var max_x = window.innerWidth;
+    var max_y = window.innerHeight;
 
-            add_tile(x, y, page_x, page_y);
+    var page_x, page_y;
+    for (page_y = 0; page_y + tile_height < max_y; page_y += half_height) {
+        for (page_x = 0; page_x + tile_width < max_x; page_x += tile_width) {
+            add_tile(0, 0, page_x + (page_y % tile_height ? half_width : 0), page_y);
         }
     }
 
     document.body.addEventListener("mousemove", function (e) {
         var map_xy = page_to_map(e.pageX, e.pageY);
+        debug("[" + map_xy[0] + "," + map_xy[1] + "]");
     }, false);
 }
